b389fff8a301089195b7dbda391f5516e72dd18c
ahkamulbd <ahkamulbd@gmail.com>
2023-08-22 17:17:27 +0600

    Click event done by using clickFunction

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-function clickFunction() {
+/* ========== Using Only Function ============ */
 
-}
-document.getElementById('btn-click').addEventListener('click', function clickFunction() {
+function clickFunction() {
     const element = document.getElementById('html-heading');
     element.innerHTML = 'Heading Changed by JavaScript:';
-})
+}
+
+/* ========== Using Event Listener ============ */
+
+// document.getElementById('btn-click').addEventListener('click', function clickFunction() {
+//     const element = document.getElementById('html-heading');
+//     element.innerHTML = 'Heading Changed by JavaScript:';
+// })
