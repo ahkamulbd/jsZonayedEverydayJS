9529e2497e82c75c993f1979646516dedebfb100
ahkamulbd <ahkamulbd@gmail.com>
2023-08-23 16:48:30 +0600

    ES 6 syntax used in reduce.js

--- js/reduce.js
+++ js/reduce.js
@@ -9,11 +9,21 @@
 
 /* ======= Using reduce with all parameters ======== */
 
-var arr = [1, 2, 3, 4, 5, 6];
+// var arr = [1, 2, 3, 4, 5, 6];
 
-var total = arr.reduce(function (sum, item, index, fullArr) {
-    console.log(`Item: ${item}, Index: ${index}, FullArray: ${fullArr} & SumArrayElements = ${sum}`);
+// var total = arr.reduce(function (sum, item, index, fullArr) {
+//     console.log(`Item: ${item}, Index: ${index}, FullArray: ${fullArr} & SumArrayElements = ${sum}`);
 
-    return sum += item;
-}, 0)
-console.log(total);
+//     return sum += item;
+// }, 0)
+// console.log(total);
+
+
+/* ======= Using reduce with ES6 Syntax ======== */
+
+let arr = [1, 2, 3, 4, 5, 6];
+
+const total = arr.reduce((sum, item) => sum += item);
+
+
+console.log(`Sum of all Array Elements = ${total}`);
